5fb2f5b94adf26b6b5a5f3c197c552cb0d3ec37d
Raj Dutta <rzskhr@outlook.com>
2018-04-04 20:28:11 -0500

    tests...

--- lyft/test/availability_test.py
+++ lyft/test/availability_test.py
@@ -29,32 +29,24 @@ token_type = res['token_type']
 
 class TestAvailability(unittest.TestCase):
     def test_get_ride_types(self):
-        ride_types = Availability(token_type, access_token).get_ride_types(37.7763, -122.3918)
-        self.assertEqual(ride_types, availability_res.ride_types_res)
-
-    def test_get_ride_types_lyft_line(self):
-        ride_types = Availability(token_type, access_token).get_ride_types(37.7763, -122.3918, 'lyft_line')
-        self.assertEqual(ride_types, availability_res.ride_types_lyft_line_res)
-
-    def test_get_ride_types_lyft(self):
-        ride_types = Availability(token_type, access_token).get_ride_types(37.7763, -122.3918, 'lyft')
-        self.assertEqual(ride_types, availability_res.ride_types_lyft_res)
-
-    def test_get_ride_types_lyft_plus(self):
-        ride_types = Availability(token_type, access_token).get_ride_types(37.7763, -122.3918, 'lyft_plus')
-        self.assertEqual(ride_types, availability_res.ride_types_lyft_plus_res)
-
-    def test_get_ride_types_lyft_premier(self):
-        ride_types = Availability(token_type, access_token).get_ride_types(37.7763, -122.3918, 'lyft_premier')
-        self.assertEqual(ride_types, availability_res.ride_types_lyft_premier_res)
-
-    def test_get_ride_types_lyft_lux(self):
-        ride_types = Availability(token_type, access_token).get_ride_types(37.7763, -122.3918, 'lyft_lux')
-        self.assertEqual(ride_types, availability_res.ride_types_lyft_lux_res)
-
-    def test_get_ride_types_lyft_luxsuv(self):
-        ride_types = Availability(token_type, access_token).get_ride_types(37.7763, -122.3918, 'lyft_luxsuv')
-        self.assertEqual(ride_types, availability_res.ride_types_lyft_luxsuv_res)
+        """
+        This test case tests the get_ride_types(self, lat, lng, ride_type=None) menthod
+        in lyft.availability.Availability module
+        """
+        self.assertEqual(Availability(token_type, access_token).get_ride_types(37.7763, -122.3918),
+                         availability_res.ride_types_res)
+        self.assertEqual(Availability(token_type, access_token).get_ride_types(37.7763, -122.3918, 'lyft_line'),
+                         availability_res.ride_types_lyft_line_res)
+        self.assertEqual(Availability(token_type, access_token).get_ride_types(37.7763, -122.3918, 'lyft'),
+                         availability_res.ride_types_lyft_res)
+        self.assertEqual(Availability(token_type, access_token).get_ride_types(37.7763, -122.3918, 'lyft_plus'),
+                         availability_res.ride_types_lyft_plus_res)
+        self.assertEqual(Availability(token_type, access_token).get_ride_types(37.7763, -122.3918, 'lyft_premier'),
+                         availability_res.ride_types_lyft_premier_res)
+        self.assertEqual(Availability(token_type, access_token).get_ride_types(37.7763, -122.3918, 'lyft_lux'),
+                         availability_res.ride_types_lyft_lux_res)
+        self.assertEqual(Availability(token_type, access_token).get_ride_types(37.7763, -122.3918, 'lyft_luxsuv'),
+                         availability_res.ride_types_lyft_luxsuv_res)
 
 
 if __name__ == '__main__':
