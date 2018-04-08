8c1071e1d3def10009fee35d27ded16a433ed6d6
Raj Dutta <rzskhr@outlook.com>
2018-04-08 16:23:25 -0500

    merging readme and url_util.py

--- lyft/util/url_util.py
+++ lyft/util/url_util.py
@@ -2,8 +2,5 @@
 PUBLIC_AUTH_URL    = "https://api.lyft.com/oauth/token"
 USER_AUTH_URL      = "https://api.lyft.com/oauth/authorize"
 AUTH_REVOKE_URL    = "https://api.lyft.com/oauth/revoke_refresh_token"
-<<<<<<< HEAD
-AVAILABILITY       = "https://api.lyft.com/v1/"
-=======
 RIDE               = "https://api.lyft.com/v1/rides"
->>>>>>> 0caca713506e6de4b89752bbd5a67ea595146b44
+AVAILABILITY       = "https://api.lyft.com/v1/"
